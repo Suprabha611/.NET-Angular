8d3ffe43c1479ea4781b7583c147380c282c10ba
suprabha611@gmail.com <suprabha611@gmail.com>
2022-09-14 21:15:17 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor (private route: ActivatedRoute) { }
+
    //TWO-WAY BINDING
    name="Dreams"
 
-  ngOnInit(): void {
+  ngOnInit() { 
+    const homeData = this.route.snapshot.data['home'];
   }
 
 }

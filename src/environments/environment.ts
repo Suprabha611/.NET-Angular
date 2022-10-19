0410a8425a5cec0488f030c76d3d3053445841da
suprabha611@gmail.com <suprabha611@gmail.com>
2022-10-19 21:26:36 +0530

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: true,
-  //userapi:"http://localhost:3000/users",
-  //cartapi:"http://localhost:3000/cart",
-  //postapi:"http://localhost:3000/posts",
-  //adminapi:"http://localhost:3000/admin",
-  postapi:"https://quiet-ridge-39486.herokuapp.com/posts",
-  //userapi:"https://quiet-ridge-39486.herokuapp.com/users",
-  userapi:"https://localhost:7026/api/registers",
-  adminapi:"https://quiet-ridge-39486.herokuapp.com/admin",
-  cartapi:"https://quiet-ridge-39486.herokuapp.com/cart"
+  userapi:"http://localhost:3000/users",
+  cartapi:"http://localhost:3000/cart",
+  postapi:"http://localhost:3000/posts",
+  adminapi:"http://localhost:3000/admin",
+  // postapi:"https://quiet-ridge-39486.herokuapp.com/posts",
+  // userapi:"https://quiet-ridge-39486.herokuapp.com/users",
+  // userapi:"https://localhost:7026/api/registers",
+  // adminapi:"https://quiet-ridge-39486.herokuapp.com/admin",
+  // cartapi:"https://quiet-ridge-39486.herokuapp.com/cart"
 };
 
 /*
